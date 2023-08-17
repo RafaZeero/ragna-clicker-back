d8bacfc7f4c25738aa76446cf40de21cc599c402
RafaZeero <rafaa99@gmail.com>
2023-08-17 09:16:14 -0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT ?? 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  // This line is important to ensure your app listens to the PORT env var
+  await app.listen(PORT, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
