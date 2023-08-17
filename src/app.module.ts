e1a1b38cf4b2157a43f11be798d3d114b483e752
RafaZeero <rafaa99@gmail.com>
2023-08-17 09:19:16 -0300

    clean up proj

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MonsterModule } from './monster/monster.module';
-import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [MonsterModule, PlayerModule],
+  imports: [],
   controllers: [AppController],
   providers: [],
 })
--- src/utils/path.ts
+++
@@ -1,3 +0,0 @@
-import * as path from 'node:path';
-
-export const monsterDBPath = path.resolve(process.cwd(), 'src/data/monsters/monsters.json');
